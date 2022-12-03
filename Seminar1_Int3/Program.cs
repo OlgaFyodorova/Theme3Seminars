3b594ee95053ba7e7d534f64dc58a364a0ed52ea
Olga <helga.yudina@gmail.com>
2022-12-03 23:03:41 +0500

    Seminar1_Int3

---
+++ Seminar1_Int3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.WriteLine("Enter day number : ");
+string EnterDay = Console.ReadLine();
+int day = Convert.ToInt32(EnterDay);
+string WeekDay = "Monday";
+
+if (day == 1 ) WeekDay = "Monday";
+if (day == 2 ) WeekDay = "Tuesday";
+if (day == 3 ) WeekDay = "Wednesday";
+if (day == 4 ) WeekDay = "Thursday";
+if (day == 5 ) WeekDay = "Friday";
+if (day == 6 ) WeekDay = "Saturday";
+if (day == 7 ) WeekDay = "Sunday";
+
+Console.WriteLine(WeekDay);
