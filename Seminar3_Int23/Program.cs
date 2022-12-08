e009bf356cb5a00b7e9493e0f5df0b87accaeef4
Olga <helga.yudina@gmail.com>
2022-12-08 21:16:03 +0500

    Seminar 3

---
+++ Seminar3_Int23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Enter number: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int i = 0;
+  int length = cube.Length;
+  while (i <  length)
+  {
+    cube[i] = Convert.ToInt32(Math.Pow(i, 3));
+    i++;
+  }
+}
+void PrintArr(int[] coll){
+  int i = coll.Length;
+  int index = 0;
+  while(index < i)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+int[] arr = new int[cube+1];
+Cube(arr);
+PrintArr(arr);
