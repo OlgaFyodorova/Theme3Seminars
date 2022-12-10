eb7df6539a7612ab63b0a16d204300ddb3db3c11
Olga <helga.yudina@gmail.com>
2022-12-10 22:25:04 +0500

    Seminar4

---
+++ Seminar4_Int28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int product = 1;
+for (int i=1; i<=N; i++)
+{
+   product = product * i;
+}
+Console.WriteLine(product);
