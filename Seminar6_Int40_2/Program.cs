b3f2d78e6e5d48d07895b46f8c9af03f98a4d4be
Olga <helga.yudina@gmail.com>
2022-12-11 22:59:41 +0500

    Seminar6

---
+++ Seminar6_Int40_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 40: Напишите программу, которая принимает на вход три
+числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника
+меньше суммы двух других сторон.
+ */
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+bool isTriangle(int a, int b, int c)
+{
+    return a+b>c && a+c>b && b+c>a;
+}
+
+if (isTriangle(a, b, c))
+{
+    Console.WriteLine("Треугольник существует");
+}
+else
+{
+    Console.WriteLine("Треугольник не существует");
+}
---
+++ Seminar6_Int45/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 45: Напишите программу, которая будет создавать
+копию заданного массива с помощью поэлементного
+копирования. 
+ */
+
+int[] array = GetArray(5, 1, 21);
+Console.WriteLine(String.Join(" ", array));
+
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int [] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+int[] Copy(int[] array)
+{
+    int[] newArray = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+Console.WriteLine($"Копия массива - {String.Join(" ", Copy(array))}");
