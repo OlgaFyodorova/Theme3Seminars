a8fd6b2a59ebae41ecf57d20ccbc03d4a85a2c31
Olga <helga.yudina@gmail.com>
2022-12-18 17:10:33 +0500

    Seminar 9

---
+++ Seminar9_Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿//Калькулятор Lite Ver.
+
+double firstNum;
+double secondNum;
+string operation;
+double result;
+
+
+Console.WriteLine("Первое число: ");
+firstNum = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Действие %, *, /, +, - : ");
+operation = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Второе число: ");
+secondNum = Convert.ToDouble(Console.ReadLine());
+
+if (operation == "+")
+{
+    result = firstNum + secondNum;
+    Console.WriteLine("Результат: " + result);
+}
+
+if (operation == "-")
+{
+    result = firstNum - secondNum;
+    Console.WriteLine("Результат: " + result);
+}
+
+if (operation == "*")
+{
+    result = firstNum * secondNum;
+    Console.WriteLine("Результат: " + result);
+}
+
+if (operation == "/")
+{
+    result = firstNum / secondNum;
+    Console.WriteLine("Результат: " + result);
+}
